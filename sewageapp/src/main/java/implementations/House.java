1fb3d6155427395f12c9e320b28a5b0e773b622a
Salad109 <szolek1337@gmail.com>
2025-02-01 15:11:56 +0100

    Start

--- sewageapp/src/main/java/implementations/House.java
+++ sewageapp/src/main/java/implementations/House.java
@@ -1,8 +1,12 @@
 package implementations;
 
 import interfaces.IHouse;
+import interfaces.IOffice;
 
+import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
 import java.rmi.server.UnicastRemoteObject;
 
 public class House extends UnicastRemoteObject implements IHouse {
@@ -12,5 +16,22 @@ public class House extends UnicastRemoteObject implements IHouse {
     @Override
     public int getPumpOut(int max) throws RemoteException {
         return 0;
+    }
+
+    public static void main(String[] args) {
+        try {
+            House house = new House();
+            Registry r = LocateRegistry.getRegistry(2000);
+            IOffice io = (IOffice) r.lookup("Office");
+            int orderStatus = io.order(house, "House1");
+
+            if (orderStatus == 0) {
+                System.out.println("Order denied");
+            } else {
+                System.out.println("Order accepted");
+            }
+        } catch (RemoteException | NotBoundException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
